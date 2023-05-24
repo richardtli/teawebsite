6f8e29fecd0c1259aa140a7b6b2395816e73a341
richardtli <43454832+richardtli@users.noreply.github.com>
2023-05-23 23:01:42 -0500

    sdgsdg

--- track-drawer.js
+++ track-drawer.js
@@ -1,32 +1,33 @@
-const track = document.getElementById("roller-coaster-u-track")
+const amusementText = document.getElementById("amusement-text")
 const container = document.querySelector(".container")
 const about = document.querySelector(".about")
-const cars = document.querySelector(".cars")
+const amusementSector = document.querySelector(".amusement-sector")
+// const cars = document.querySelector(".cars")
 
 
 
 
 
-const trackLength = track.getTotalLength();
-track.style.strokeDasharray = trackLength
-track.style.strokeDashoffset = trackLength
+const amusementTextLength = amusementText.getTotalLength();
+amusementText.style.strokeDasharray = amusementTextLength
+amusementText.style.strokeDashoffset = amusementTextLength
 
 let percentage = 0;
 let drawLength = 0;
 
-const showTrackProgression = () => {
-    if(container.scrollTop > window.innerHeight && container.scrollTop < about.offsetHeight ){
+const showScrollProgressionAmusement = () => {
+    if(container.scrollTop > window.innerHeight && container.scrollTop < amusementSector.offsetHeight ){
         // percentage = (1 - (about.offsetHeight + window.innerHeight - container.scrollTop )/about.offsetHeight).toFixed(3)
-        percentage = (container.scrollTop - window.innerHeight) / (about.offsetHeight - window.innerHeight)
-        drawLength = trackLength * percentage
-        track.style.strokeDashoffset =  drawLength+trackLength
+        percentage = (container.scrollTop - window.innerHeight) / (amusementSector.offsetHeight - window.innerHeight)
+        drawLength = amusementTextLength * percentage
+        amusementText.style.strokeDashoffset =  amusementTextLength-drawLength
     }
 
-    else if(container.scrollTop > about.offsetHeight){
-        container.removeEventListener('scroll', showTrackProgression)
-        track.style.strokeDashoffset =  2*trackLength
-        cars.style.opacity = "1"
-    }
+    // else if(container.scrollTop > about.offsetHeight){
+    //     container.removeEventListener('scroll', showTrackProgression)
+    //     track.style.strokeDashoffset =  2*trackLength
+    //     cars.style.opacity = "1"
+    // }
 }
 
-container.addEventListener('scroll', showTrackProgression)
+container.addEventListener('scroll', showScrollProgressionAmusement)
